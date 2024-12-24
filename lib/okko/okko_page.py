797568ed11b981d05af3776ba496485a084d249d
VlAdOsAn <v.chindyaykin@gost.team>
2024-12-24 16:57:10 +0300

    Новая ветка для работы Севы

--- lib/okko/okko_page.py
+++ lib/okko/okko_page.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from utils.page import Page
 
-
+#Сперма
 class OkkoPage(Page):
     def __init__(self, driver: WebDriver):
         super().__init__(driver)
